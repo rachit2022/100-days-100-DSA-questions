e12223b749fa554aa4a3a8f49dcb32727d93b943
Rachit <rachitrajgupta2020@gmail.com>
2024-03-18 20:50:06 +0530

    adding files

---
+++ MaximumSubarray.java
@@ -0,0 +1,21 @@
+public class MaximumSubarray {
+    public static int maximumSubarray(int[] nums){
+        int maxSum=Integer.MIN_VALUE;
+        int currentSum=0;
+        for(int i=0;i<nums.length;i++){
+            currentSum+=nums[i];
+            if(currentSum>maxSum){
+                maxSum=currentSum;
+            }
+            if(currentSum<0){
+                currentSum=0;
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args){
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maximumSubarray(nums));
+
+    }
+}
