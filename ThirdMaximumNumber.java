17605f97e9bc8ccbe34b592d6a1ade1459827537
Rachit <rachitrajgupta2020@gmail.com>
2024-04-04 18:30:05 +0530

    adding files

---
+++ ThirdMaximumNumber.java
@@ -0,0 +1,28 @@
+public class ThirdMaximumNumber {
+
+    public static int thirdMax(int[] arr) {
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : arr) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3))
+                continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 2, 3, 1 };
+        System.out.println(thirdMax(arr));
+    }
+}
