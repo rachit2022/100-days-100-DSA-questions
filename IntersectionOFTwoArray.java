b477f7262164ff0c47d4b58b97c8aaccb9f8d9ed
Rachit <rachitrajgupta2020@gmail.com>
2024-04-09 14:16:22 +0530

    adding files

---
+++ IntersectionOFTwoArray.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IntersectionOFTwoArray {
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                arr.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] output = new int[arr.size()];
+        int k = 0;
+        while(k < arr.size()){
+            output[k] = arr.get(k);
+            k++;
+        }
+        return output;
+    }
+    public static void main(String[] args){
+        int[] nums1={1,2,2,1};
+        int[] nums2={2,2};
+        System.out.println(intersect(nums1, nums2));
+    }
+}
