3a16e6dfc187732f5a12bc40b51efef60a1f7e0f
unknown <rachitrajgupta2020@gmail.com>
2024-05-15 09:05:15 +0530

    adding files

---
+++ AddBinary.java
@@ -0,0 +1,25 @@
+public class AddBinary {
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0)
+                carry += a.charAt(i--) - '0';
+            if (j >= 0)
+                carry += b.charAt(j--) - '0';
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a = "10";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+}
