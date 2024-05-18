6288bf8ee41f566829a894662e8108d0cd8508d4
unknown <rachitrajgupta2020@gmail.com>
2024-05-18 22:09:07 +0530

    adding files

---
+++ DistributeCoinsInBinaryTree.java
@@ -0,0 +1,21 @@
+public class DistributeCoinsInBinaryTree {
+    private int moves = 0;
+
+    private int dfs(TreeNode node) {
+        if (node == null)
+            return 0;
+        int left_excess = dfs(node.left);
+        int right_excess = dfs(node.right);
+        moves += Math.abs(left_excess) + Math.abs(right_excess);
+        return node.val + left_excess + right_excess - 1;
+    }
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return moves;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
