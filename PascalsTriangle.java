9cbd4f701c1a2535004fe05ad5cf18b62b59f1e8
unknown <rachitrajgupta2020@gmail.com>
2024-05-19 08:50:03 +0530

    adding files

---
+++ PascalsTriangle.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        long prev = 1;
+        for (int k = 1; k <= rowIndex; k++) {
+            long next_val = prev * (rowIndex - k + 1) / k;
+            res.add((int) next_val);
+            prev = next_val;
+        }
+        return res;
+    }
+    public static void main(String[] args){
+
+    }
+}
