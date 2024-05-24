05bae72626c043f501af8c2609bf33b26dba2774
unknown <rachitrajgupta2020@gmail.com>
2024-05-24 20:34:54 +0530

    adding files

---
+++ ValidWord.java
@@ -0,0 +1,28 @@
+public class ValidWord {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                vowelCount++;
+            } else if ((c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') && "aeiouAEIOU".indexOf(c) == -1) {
+                consonantCount++;
+            }
+        }
+
+        return vowelCount > 0 && consonantCount > 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
