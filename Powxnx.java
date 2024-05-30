bc4f879e2aeb002318a735be79e9aaa0eee0c0f0
unknown <rachitrajgupta2020@gmail.com>
2024-05-30 21:53:41 +0530

    adding files

---
+++ Powxnx.java
@@ -0,0 +1,27 @@
+public class Powxnx {
+    public double myPow(double x, int n) {
+
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+
+        double pow = 1;
+
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                pow *= x;
+            }
+
+            x *= x;
+            n >>>= 1;
+
+        }
+
+        return pow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
